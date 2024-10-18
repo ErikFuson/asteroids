43f3efcc8333b7d3e16d05376e0c9dc95407987d
ErikFuson <eriksfuson@gmail.com>
2024-10-18 11:33:38 -0500

    added framerate and circleshape class

--- main.py
+++ main.py
@@ -6,14 +6,25 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # get the delta time from  the framerate
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((BACKGROUND_COLOR))
         pygame.display.flip()
+
+        dt = (clock.tick(60) / 1000)
+        
+
+        
+        
+        
 
 if __name__ == "__main__":
     main()
